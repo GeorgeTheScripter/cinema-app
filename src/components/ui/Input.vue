9538c85ee88b49cc1f56e7fee886fc9d813ce493
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2025-11-17 09:59:51 +0300

    feature - base search setup
    - create custom input
    - create grid for search results
    - base set up for search.store

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
+import SearchIcon from './SearchIcon.vue';
+
 const props = defineProps<{
   modelValue: string;
   placeholder: string;
 }>();
 
-defineEmits<{
+const emit = defineEmits<{
   'update:modelValue': [value: string];
 }>();
+
+const handleInput = (event: Event) => {
+  emit('update:modelValue', (event.target as HTMLInputElement).value);
+};
 </script>
 
 <template>
-  <input
-    :placeholder="props.placeholder"
-    type="text"
-    :value="props.modelValue"
-    class="w-full py-3 px-4 text-gray-800 bg-white border border-gray-300 hover:bg-gray-50 hover:border-gray-400 active:bg-gray-100 placeholder:text-gray-500 rounded-lg focus:border-blue-600 outline-none transition-colors"
-    @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
-  />
+  <div class="relative w-full max-w-sm">
+    <input
+      @input="handleInput"
+      type="text"
+      class="w-full bg-gray-800 text-white border border-gray-700 rounded-xl py-3 pl-12 pr-4 shadow-lg focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-gray-600 placeholder-gray-400"
+      :placeholder="props.placeholder"
+    />
+    <SearchIcon />
+  </div>
 </template>
