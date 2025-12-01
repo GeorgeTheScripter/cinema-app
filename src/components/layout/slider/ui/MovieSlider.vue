96e72caaa81fff97723b1cf5a12aa33d77f69201
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2025-12-01 13:02:40 +0300

    fix - fix layout main page

--- src/components/layout/slider/ui/MovieSlider.vue
+++ src/components/layout/slider/ui/MovieSlider.vue
@@ -18,15 +18,15 @@ const breakpoints = {
     spaceBetween: 8,
   },
   640: {
-    slidesPerView: 2,
+    slidesPerView: 3,
     spaceBetween: 8,
   },
   768: {
-    slidesPerView: 3,
+    slidesPerView: 4,
     spaceBetween: 12,
   },
   1024: {
-    slidesPerView: 4,
+    slidesPerView: 5,
     spaceBetween: 12,
   },
   1440: {
